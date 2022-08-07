806328f58a39a22aa9d24fe2a4001b3e284c8238
Alex Mitchell <alex.d.mitchell@protonmail.com>
2022-08-07 18:20:59 +0100

    Play/Pause video

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,11 +1,36 @@
+import React, { ForwardedRef } from 'react'
+import styles from './VideoPlayer.module.css'
+
 interface Props {
   src: string
 }
 
 export const VideoPlayer = ({ src }: Props) => {
+  const ref = React.useRef<HTMLVideoElement>(null)
+  const [isPlaying, setIsPlaying] = React.useState(false)
+
+  const toggleVideo = () => {
+    if (!ref.current) {
+      return
+    }
+    if (ref.current.paused) {
+      ref.current.play()
+      setIsPlaying(true)
+      return
+    }
+    ref.current.pause()
+    setIsPlaying(false)
+  }
+
   return (
-    <video>
-      <source src={src} />
-    </video>
+    <div className={styles.container}>
+      <video ref={ref} height={'100%'} width={'100%'}>
+        <source src={src} />
+      </video>
+
+      <button className={styles.toggleVideoButton} onClick={toggleVideo}>
+        {isPlaying ? '❚ ❚' : '►'}
+      </button>
+    </div>
   )
 }
